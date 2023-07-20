a4637fa261980b88673f001284b8a3a2976558e6
amitagondi <47047343+amitagondi@users.noreply.github.com>
2023-07-21 03:24:26 +0530

    Add files via upload

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { IntroPage } from './Intro.jsx'
+
+ReactDOM.createRoot( 
+  document.querySelector('#root')
+).render(<IntroPage />)
